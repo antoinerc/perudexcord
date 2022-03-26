a25780f2c4493f5c49f88751cb1c722cde49db4b
Antoine <antoine.richer.cloutier@gmail.com>
2022-03-25 21:38:02 -0400

    Initial commit

---
+++ lib/perudo_cord.ex
@@ -0,0 +1,18 @@
+defmodule PerudoCord do
+  @moduledoc """
+  Documentation for `PerudoCord`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PerudoCord.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/perudo_cord_test.exs
@@ -0,0 +1,8 @@
+defmodule PerudoCordTest do
+  use ExUnit.Case
+  doctest PerudoCord
+
+  test "greets the world" do
+    assert PerudoCord.hello() == :world
+  end
+end
