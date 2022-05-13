c1f446c95cdac2e9ef820392c06416d334a97db1
Antoine <antoine.richer.cloutier@gmail.com>
2022-05-12 21:50:38 -0400

    Implement remaining missing NotifierServer callbacks

--- lib/player_notifier.ex
+++ lib/player_notifier.ex
@@ -4,99 +4,172 @@ defmodule PerudoCord.PlayerNotifier do
   alias Nostrum.Api
   alias PerudoCord.{Games, Game, InteractiveMessageHistory}
 
-  def illegal_move(game_id, player_id) do
+  def illegal_move(game_id, recipient_id) do
+    with {:ok, dm_channel} <- Api.create_dm(recipient_id),
+         %Game{game_name: name} <- Games.get(game_id) do
+      Api.create_message(
+        dm_channel.id,
+        "Illegal move for game #{name}!"
+      )
+
+      move(game_id, recipient_id)
+    end
   end
 
-  def invalid_bid(game_id, player_id) do
+  def invalid_bid(game_id, recipient_id) do
+    with {:ok, dm_channel} <- Api.create_dm(recipient_id),
+         %Game{game_name: name} <- Games.get(game_id) do
+      Api.create_message(
+        dm_channel.id,
+        "Invalid bet for game #{name}!"
+      )
+
+      move(game_id, recipient_id)
+    end
   end
 
-  def loser(game_id, player_id, loser_id) do
+  def loser(game_id, recipient_id, loser_id) do
+    with {:ok, dm_channel} <- Api.create_dm(recipient_id),
+         %Game{game_name: name} <- Games.get(game_id) do
+      Api.create_message(
+        dm_channel.id,
+        "Player #{user(loser_id)} has been eliminated from game #{name}!"
+      )
+    end
   end
 
-  def move(game_id, player_id) do
-    with {:ok, dm_channel} <- Api.create_dm(player_id),
+  def unauthorized_move(game_id, recipient_id) do
+    with {:ok, dm_channel} <- Api.create_dm(recipient_id),
+         %Game{game_name: name} <- Games.get(game_id) do
+      Api.create_message(
+        dm_channel.id,
+        "It is not your turn to play in game #{name}!"
+      )
+    end
+  end
+
+  def move(game_id, recipient_id) do
+    with {:ok, dm_channel} <- Api.create_dm(recipient_id),
          %Game{game_name: name} <- Games.get(game_id),
          {:ok, message} <-
            Api.create_message(
              dm_channel.id,
-             "It is your turn to play in game #{name}. Reply to this message with your new bid in the format [count, die] or react with either #{%Nostrum.Struct.Emoji{name: "👎"}} for Dudo or #{%Nostrum.Struct.Emoji{name: "👌"}} for Calza."
+             "It is your turn to play in game #{name}. Reply to this message with your new bid in the format [count, die] or react with either #{emoji("👎")} for Dudo or #{emoji("👌")} for Calza."
            ) do
-      InteractiveMessageHistory.insert(player_id, message.id, game_id)
+      InteractiveMessageHistory.insert(recipient_id, message.id, game_id)
     else
       error -> error
     end
   end
 
-  def new_bid(game_id, player_id, {count, die}) do
-  end
-
-  def new_hand(game_id, player_id, %Perudex.Hand{dice: dice}) do
-    with {:ok, dm_channel} <- Api.create_dm(player_id),
-         %Game{game_name: name} <- Games.get(game_id),
-         {:ok, message} <-
-           Api.create_message(
-             dm_channel.id,
-             "Your new hand for game #{name} is #{inspect(dice)}"
-           ) do
-      InteractiveMessageHistory.insert(player_id, message.id, game_id)
+  def new_hand(game_id, recipient_id, %Perudex.Hand{dice: dice}) do
+    with {:ok, dm_channel} <- Api.create_dm(recipient_id),
+         %Game{game_name: name} <- Games.get(game_id) do
+      Api.create_message(
+        dm_channel.id,
+        "Your new hand for game #{name} is #{inspect(dice)}"
+      )
     end
   end
 
-  def reveal_players_hands(game_id, player_id, hands) do
-  end
+  def reveal_players_hands(game_id, recipient_id, hands) do
+    msg =
+      Enum.reduce(hands, fn hand, acc ->
+        "#{acc} #{user(hand.player_id)}: #{inspect(hand.dice)}\n"
+      end)
 
-  def start_game(game_id, player_id, participating_players) do
-    with {:ok, dm_channel} <- Api.create_dm(player_id),
-         %Game{game_name: name} <- Games.get(game_id),
-         {:ok, message} <-
-           Api.create_message(
-             dm_channel.id,
-             "Game #{name} has started! Players: #{Enum.map(participating_players, fn p -> "#{%Nostrum.Struct.User{id: p}}" end)}"
-           ) do
-      InteractiveMessageHistory.insert(player_id, message.id, game_id)
+    with {:ok, dm_channel} <- Api.create_dm(recipient_id),
+         %Game{game_name: name} <- Games.get(game_id) do
+      Api.create_message(
+        dm_channel.id,
+        "The hands for the latest round of game #{name} were: \n #{msg}"
+      )
     end
   end
 
-  def successful_dudo(game_id, player_id) do
+  def start_game(game_id, recipient_id, participating_players) do
+    with {:ok, dm_channel} <- Api.create_dm(recipient_id),
+         %Game{game_name: name} <- Games.get(game_id) do
+      Api.create_message(
+        dm_channel.id,
+        "Game #{name} has started! Players: #{Enum.map(participating_players, fn p -> "#{user(p)}" end)}"
+      )
+    end
   end
 
-  def successful_calza(game_id, player_id) do
+  def last_move(game_id, recipient_id, move_initiator, {:calza, true}) do
+    with {:ok, dm_channel} <- Api.create_dm(recipient_id),
+         %Game{game_name: name} <- Games.get(game_id) do
+      Api.create_message(
+        dm_channel.id,
+        "Player #{user(move_initiator)} in game #{name} called CALZA and was RIGHT!"
+      )
+    end
   end
 
-  def unauthorized_move(game_id, player_id) do
+  def last_move(game_id, recipient_id, move_initiator, {:calza, false}) do
+    with {:ok, dm_channel} <- Api.create_dm(recipient_id),
+         %Game{game_name: name} <- Games.get(game_id) do
+      Api.create_message(
+        dm_channel.id,
+        "Player #{user(move_initiator)} in game #{name} called DUDO and was WRONG!"
+      )
+    end
   end
 
-  def unsuccessful_calza(game_id, player_id) do
+  def last_move(game_id, recipient_id, move_initiator, {:dudo, true}) do
+    with {:ok, dm_channel} <- Api.create_dm(recipient_id),
+         %Game{game_name: name} <- Games.get(game_id) do
+      Api.create_message(
+        dm_channel.id,
+        "Player #{user(move_initiator)} in game #{name} called DUDO and was RIGHT!"
+      )
+    end
   end
 
-  def unsuccessful_dudo(game_id, player_id) do
+  def last_move(game_id, recipient_id, move_initiator, {:dudo, false}) do
+    with {:ok, dm_channel} <- Api.create_dm(recipient_id),
+         %Game{game_name: name} <- Games.get(game_id) do
+      Api.create_message(
+        dm_channel.id,
+        "Player #{user(move_initiator)} in game #{name} called DUDO and was WRONG!"
+      )
+    end
+  end
+
+  def last_move(game_id, recipient_id, move_initiator, {:outbid, {count, die}}) do
+    with {:ok, dm_channel} <- Api.create_dm(recipient_id),
+         %Game{game_name: name} <- Games.get(game_id) do
+      Api.create_message(
+        dm_channel.id,
+        "Player #{user(move_initiator)} in game #{name} has raised the bid to #{count} x #{die}!"
+      )
+    end
   end
 
   def winner(game_id, winner_id, winner_id) do
     with {:ok, dm_channel} <- Api.create_dm(winner_id),
-         %Game{game_name: name} <- Games.get(game_id),
-         {:ok, _} <-
-           Api.create_message(
-             dm_channel.id,
-             "#{%Nostrum.Struct.Emoji{name: "🏆"}} Congratulation on WINNING game #{name} #{%Nostrum.Struct.Emoji{name: "🏆"}}"
-           ) do
-      InteractiveMessageHistory.insert(winner_id, winner_id, game_id)
+         %Game{game_name: name} <- Games.get(game_id) do
+      Api.create_message(
+        dm_channel.id,
+        "#{emoji("🏆")} Congratulation on WINNING game #{name} #{emoji("🏆")}"
+      )
     end
   end
 
-  def winner(game_id, player_id, winner_id) do
-    with {:ok, dm_channel} <- Api.create_dm(player_id),
-         %Game{game_name: name} <- Games.get(game_id),
-         {:ok, message} <-
-           Api.create_message(
-             dm_channel.id,
-             "#{%Nostrum.Struct.Emoji{name: "🏆"}} Player #{%Nostrum.Struct.User{id: winner_id}} has WON game #{name} #{%Nostrum.Struct.Emoji{name: "🏆"}}"
-           ) do
-      InteractiveMessageHistory.insert(player_id, message.id, game_id)
+  def winner(game_id, recipient_id, winner_id) do
+    with {:ok, dm_channel} <- Api.create_dm(recipient_id),
+         %Game{game_name: name} <- Games.get(game_id) do
+      Api.create_message(
+        dm_channel.id,
+        "#{emoji("🏆")} Player #{user(winner_id)} has WON game #{name} #{emoji("🏆")}"
+      )
     end
   end
 
-  def player_spec(game_id, player_id) do
-    %{id: player_id, callback_mod: __MODULE__, callback_arg: game_id}
-  end
+  def player_spec(game_id, recipient_id),
+    do: %{id: recipient_id, callback_mod: __MODULE__, callback_arg: game_id}
+
+  defp user(id), do: %Nostrum.Struct.User{id: id}
+  defp emoji(emoji), do: %Nostrum.Struct.Emoji{name: emoji}
 end
